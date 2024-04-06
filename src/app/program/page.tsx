1bb734343b4e041af1e59be2e0276784cb9b7f8e
bozzhik <bozzhik@ya.ru>
2024-04-07 02:01:58 +0300

    add ebuchii math ebati ego v rot random @bozzhik

--- src/app/program/page.tsx
+++ src/app/program/page.tsx
@@ -5,6 +5,7 @@ import Programs from '@/components/Programs'
 export default function Program() {
   return (
     <>
+      {Math.random()}
       <Link prefetch={false} href={'/'} className="duration-200 block w-fit mt-5 mx-auto hover:text-custom-teal">
         На главную
       </Link>
